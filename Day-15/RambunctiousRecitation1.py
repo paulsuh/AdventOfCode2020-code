1c86b8fd4e09e5abb8e5314723e0cbda2f7a7115
Paul Suh <paul@mspex.net>
2020-12-15 01:16:03 -0500

    Day 15 solution
    This was pretty easy

---
+++ Day-15/RambunctiousRecitation1.py
@@ -0,0 +1,42 @@
+#!/Users/plsuh/Projects/AdventOfCode2020-venv/bin/python
+
+from sys import stdin
+
+# read in input text
+inputText = stdin.read().splitlines()
+
+currentTurnNum = 1
+spokenNums={}
+prevNumStr = "0"
+prevNumTurnNum = -1
+
+# prime the dict
+for n in inputText[0].split(","):
+    spokenNums[n] = currentTurnNum
+    prevNumStr = n
+    prevNumTurnNum = -1
+    print(currentTurnNum,n)
+    currentTurnNum += 1
+
+# next number is always "0"
+# prevNumStr = "0"
+# prevNumTurnNum = spokenNums.get(prevNumStr,-1)
+# print(currentTurnNum,prevNumStr)
+# currentTurnNum += 1
+
+# print(spokenNums)
+while currentTurnNum <= 2020:
+    prevTurnNum = spokenNums.get(prevNumStr,-1)
+    print(f"   prevNumStr={prevNumStr}  prevNumTurnNum={prevNumTurnNum}")
+    if prevNumTurnNum == -1:
+        # number has never been spoken
+        currNumStr = "0"
+    else:
+        currNumStr = str(currentTurnNum - prevNumTurnNum - 1)
+    
+    print(f"{currentTurnNum} prevNumStr={prevNumStr} prevNumTurnNum={prevNumTurnNum} currNumStr={currNumStr}")
+    spokenNums[prevNumStr]=currentTurnNum - 1
+    prevNumStr = currNumStr
+    prevNumTurnNum = spokenNums.get(currNumStr,-1)
+    currentTurnNum += 1
+
